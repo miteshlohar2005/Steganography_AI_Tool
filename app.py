6312ce5b0a458509d26f5eb0d80b3d05eb3d56f3
miteshlohar2005 <miteshlohar2019@gmail.com>
2025-12-28 17:40:37 +0530

    Remove debug mode from app.run()

--- app.py
+++ app.py
@@ -270,5 +270,4 @@ def decode_audio_action():
 # ================= MAIN =================
 
 if __name__ == '__main__':
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    app.run()
